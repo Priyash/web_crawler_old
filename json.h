10059aa74ac69a1a059ac2da53ee229be818481d
Priyash <Priyash@users.noreply.github.com>
2015-12-23 16:39:14 +0530

    Create json.h
    Small Json Parser which only parse the customized parser for inverted index basically json format is being used for word storage.

---
+++ json.h
@@ -0,0 +1,47 @@
+#ifndef JSON_H
+#define JSON_H
+
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<fstream>
+using namespace std;
+
+struct Word
+{
+	int id;
+	string word;
+	vector<int>freq;
+	vector<int>docList;
+};
+
+
+class Json
+{
+	string file;
+	string _path="C:\\Users\\PRIYASH_11\\Desktop\\XML_data\\";
+	string path;
+	string path2;
+	ifstream reader;
+	vector<Word>json_data;
+	ofstream writer;
+public:
+	Json(string file)
+	{
+		this->file = file;
+		path = _path + file;
+		path2 = _path + file;
+		reader.open(path.c_str(), ios::in);
+	}
+
+	void read();
+	void write(vector<Word>w);
+	void addWord(string w, int freq, int docID);
+	vector<Word>getJsonData(){ return json_data; }
+
+private:
+	bool isExist();
+};
+
+#endif
