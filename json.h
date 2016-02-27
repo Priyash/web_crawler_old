3dd266b2689921b8f5dd2c4da7e2fa838c718ad8
Priyash <Priyash@users.noreply.github.com>
2016-02-28 02:15:44 +0530

    Refreshed Version of JSON
    I used tree model for parsing json files

--- json.h
+++ json.h
@@ -1,47 +1,177 @@
-#ifndef JSON_H
-#define JSON_H
+#include"Json.h"
 
-#include<iostream>
-#include<string>
-#include<algorithm>
-#include<vector>
-#include<fstream>
-using namespace std;
-
-struct Word
+void jsonObject::add(string key, jsonObject* obj)
 {
-	int id;
-	string word;
-	vector<int>freq;
-	vector<int>docList;
-};
+	objChild.insert(pair<string, jsonObject*>(key, obj));
+}
 
 
-class Json
+void jsonObject::add(string key, string value)
 {
-	string file;
-	string _path="XML_data\\";
-	string path;
-	string path2;
-	ifstream reader;
-	vector<Word>json_data;
-	ofstream writer;
-public:
-	Json(string file)
+	sChild.insert(pair<string, string>(key, value));
+}
+
+void jsonObject::add(string key, double value)
+{
+	dChild.insert(pair<string, double>(key, value));
+}
+
+void jsonObject::add(string key, bool value)
+{
+	bChild.insert(pair<string, bool>(key, value));
+}
+
+
+void jsonObject::add(string key, vector<jsonObject*>objArrayvalue)
+{
+	objArrayChild.insert(pair<string, vector<jsonObject*>>(key, objArrayvalue));
+}
+
+void jsonObject::add(string key, vector<string>strArrayvalue)
+{
+	strArrayChild.insert(pair<string, vector<string>>(key, strArrayvalue));
+}
+
+void jsonObject::add(string key, vector<double>dArrayvalue)
+{
+	dArrayChild.insert(pair<string, vector<double>>(key, dArrayvalue));
+}
+
+
+bool Json::isDigit(string b)
+{
+	b = b.substr(0, b.find_last_of(","));
+	return b.find_first_not_of("0123456789.") == string::npos;
+}
+
+
+bool Json::isObject(char c)
+{
+	return c == '{';
+}
+
+bool Json::isBool(string b)
+{
+	b = b.substr(0, b.find_last_of(","));
+	return b == "true" || b == "false";
+}
+
+bool Json::isArray(char c)
+{
+	return c == '[';
+}
+
+bool Json::isString(char c)
+{
+	return c == '"';
+}
+
+void Json::parseData(jsonObject* node,vector<string>data,int index)
+{
+	if (node == NULL)return;
+	string line = data[index];
+	
+	string a = line.substr(line.find_first_of("\"") + 1, line.find(":") - line.find_first_of("\"") -2 );
+	string b = line.substr(line.find(":") + 1, line.find_last_of("\"") - line.find(":") -1);
+	if (line == "}"||line=="},")
 	{
-		this->file = file;
-		path = _path + file;
-		path2 = _path + file;
-		reader.open(path.c_str(), ios::in);
+		index++;
+		if (Array)
+		{
+			arrayOBJ.push_back(node);
+			//node->getParent()->add(k, arrayOBJ);
+			parseData(node->getParent(), data, index);
+		}
+		else
+		{
+			if (node->getParent() != NULL)
+			{
+				parseData(node->getParent(), data, index);
+			}
+		}
 	}
 
-	void read();
-	void write(vector<Word>w);
-	void addWord(string w, int freq, int docID);
-	vector<Word>getJsonData(){ return json_data; }
+	if (line == "]")
+	{
+		node->add(k, arrayOBJ);
+		Array = false;
+		k = "";
+		arrayOBJ.clear();
+		index++;
+		parseData(node, data, index);
+	}
 
-private:
-	bool isExist();
-};
+	if (isString(b[0]))
+	{
+		string str = b.substr(1, b.length() - 1);
+		cout << a << " " << str << endl;
+		node->add(a, str);
+		index++;
+		parseData(node, data, index);
+	}
+	else if (isDigit(b))
+	{
+		b = b.substr(0, b.find_last_of(","));
+		double d = atof(b.c_str());
+		cout << a << " " << d << endl;
+		node->add(a, d);
+		index++;
+		parseData(node, data, index);
+	}
+	else if (isBool(b))
+	{
+		b = b.substr(0, b.find_last_of(","));
+		bool bl = (bool)b.c_str();
+		cout << a << " " << bl << endl;
+		node->add(a, bl);
+		parseData(node, data, index++);
+	}
+	else if (isObject(b[0]))
+	{
+		index++;
+		if (!Array)
+		{
+			jsonObject* j = new jsonObject(node);
+			node->add(a, j);
+			parseData(j, data, index);
+		}
+		else
+		{
+			jsonObject* j = new jsonObject(node);
+			parseData(j, data, index);
+		}
+	}
+	else if (isArray(b[0]))
+	{
+		index++;
+		Array = true;
+		k = a;
+		parseData(node, data, index);
+	}
+	
+}
 
-#endif
+
+
+void Json::readJSON()
+{
+	string line;
+	int startSecondBracket = 0;
+	int endSecondBracket = 0;
+	while (getline(jsonReader, line))
+	{
+		line.erase(remove(line.begin(), line.end(), '\t'), line.end());
+		line.erase(remove(line.begin(), line.end(), ' '), line.end());
+		data.push_back(line);
+	}
+
+	parseData(root, data, 1);
+
+}
+
+
+
+
+
+
+
